3f871739d1ce3a5983ce6aed8c84ab6a912832e8
Jezreel Tan <jezdtanslc90@gmail.com>
2025-03-13 09:27:15 +0800

    202507 en

--- src/data/notifications.ts
+++ src/data/notifications.ts
@@ -2,6 +2,24 @@ import type { Notification } from "@/types/notification"
 
 const notifications: Notification[] = [
     {
+        id: 'd1',
+        date: '2025-03-13T00:00:00-00:08',
+        title: "OCLM - July 2025 (English)",
+        subheader: 'Schedule is added',
+        link: '?month=202507&lang=en',
+        isLink: true,
+        icon: "new"
+    },
+    {
+        id: 'd2',
+        date: '2025-03-13T00:00:00-00:08',
+        title: "OCLM - August 2025 (English)",
+        subheader: 'Schedule is added',
+        link: '?month=202508&lang=en',
+        isLink: true,
+        icon: "new"
+    },
+    {
         id: 'c2',
         date: '2025-03-11T00:00:00-00:08',
         title: "OCLM - Mayo 2025 (Hiligaynon)",
@@ -10,7 +28,7 @@ const notifications: Notification[] = [
         isLink: true,
         icon: "new"
     },
-    {   
+    {
         id: 'c3',
         date: '2025-03-11T00:00:00-00:08',
         title: "OCLM - Hunyo 2025 (Hiligaynon)",
@@ -28,7 +46,7 @@ const notifications: Notification[] = [
         isLink: true,
         icon: "new"
     },
-    {   
+    {
         id: 'c5',
         date: '2025-03-11T00:00:00-00:08',
         title: "OCLM - Hunio 2025 (Iloko)",
@@ -46,7 +64,7 @@ const notifications: Notification[] = [
         isLink: true,
         icon: "new"
     },
-    {   
+    {
         id: 'c7',
         date: '2025-03-11T00:00:00-00:08',
         title: "OCLM - Hunyo 2025 (Pangasinan)",
